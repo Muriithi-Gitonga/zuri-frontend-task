6355d59d674b706d35e15d1d234242c6b6319b92
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-09-10 10:08:01 +0300

    complete

---
+++ main.js
@@ -0,0 +1,30 @@
+// update time
+function updateUTCTime() {
+  const currentUTCTime = new Date().getTime();
+  document.querySelector('[data-testid="currentUTCTime"]').textContent =
+    `Current UTC Time: ${currentUTCTime} milliseconds`;
+}
+
+// update current week
+function updateDayOfWeek() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDayIndex = new Date().getDay(); // 0 for Sunday, 1 for Monday, etc.
+  const currentDayOfWeek = daysOfWeek[currentDayIndex];
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+    `Current Day: ${currentDayOfWeek}`;
+}
+
+// Call the functions initially to display data
+updateUTCTime();
+updateDayOfWeek();
+
+// Update the time every second (1000 milliseconds)
+setInterval(updateUTCTime, 1000);
